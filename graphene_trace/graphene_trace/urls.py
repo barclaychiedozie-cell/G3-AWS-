0741e6e60f08aeb22abba00e3256829320db8944
emmyfreshh <emma1top@gmail.com>
2026-04-15 10:48:57 +0100

    updated settings and url

--- graphene_trace/graphene_trace/urls.py
+++ graphene_trace/graphene_trace/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -61,3 +63,6 @@ urlpatterns = [
     path("", include("patients.urls")),
     path("", include("clinicians.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
